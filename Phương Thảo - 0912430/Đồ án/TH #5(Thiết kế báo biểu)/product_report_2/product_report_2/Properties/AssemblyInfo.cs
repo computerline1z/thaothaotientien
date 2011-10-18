5619eb35c89460ba0cb14585212f1b22edd9fae4
hathao298@gmail.com <hathao298@gmail.com@4f8ca73e-40c0-7a3e-fdf5-1de95dece025>
2011-10-18 15:05:08 +0000

    submit product_report_2 Author:JML

---
+++ Phương Thảo - 0912430/Đồ án/TH #5(Thiết kế báo biểu)/product_report_2/product_report_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("product_report_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("product_report_2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2ca1efe-1bc3-4acf-a21d-865ae44f3787")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
